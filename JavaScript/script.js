ae9e793b37e225f24b6ee3bf4e61ae6c4e17d1dd
sara10aguilar12-arch <sara10aguilar12@gmail.com>
2025-10-20 20:17:00 -0600

    Fix element ID typos and update query selectors

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,18 +1,17 @@
-const lightbox = document.getElementByld('lightbox');
-const imagenAmpliada = document.getElementByld('imagen-ampliada');
-const caption = document.getElementByld('caption');
+const lightbox = document.getElementById('lightbox');
+const imagenAmpliada = document.getElementById('imagen-ampliada');
+const caption = document.getElementById('caption');
 const cerrar = document.querySelector('.cerrar');
-const imagenes = document.querySelectorAll('.galeria.img');
+const imagenes = document.querySelectorAll('.galeria img');
 
-imagenes.forEach(img=>{
-	img.addEventListener('click', ()=>{
-		lightbox.style.display='block';
-		imagenAmpliada.src = img.src;
-		caption.textContent = img.alt;
-	});
-
+imagenes.forEach(img => {
+  img.addEventListener('click', () => {
+    lightbox.style.display = 'block';
+    imagenAmpliada.src = img.src;
+    caption.textContent = img.alt;
+  });
 });
 
-cerrar.addEventListener('click',() => {
-	lightbox.style.display = 'none';
+cerrar.addEventListener('click', () => {
+  lightbox.style.display = 'none';
 });
